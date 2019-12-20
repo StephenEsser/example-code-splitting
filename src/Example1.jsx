886f7475e459cf0958a4f3e1eba3327b4e702745
Stephen Esser <Stephen.Esser@gmail.com>
2019-12-20 16:02:14 -0600

    Update source url

--- src/Example1.jsx
+++ src/Example1.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Example = () => (
   <div>
     Example 1
-    <iframe src="/?example=2" title="happy hacking" />
+    <iframe src="?example=2" title="happy hacking" />
   </div>
 );
 
